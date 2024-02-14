6a80e98e96e0c723cc5c711616aa797fd01543ac
unknown <jomirhossain15@gmail.com>
2024-02-15 02:40:09 +0600

    add

---
+++ alpha.js
@@ -0,0 +1,100 @@
+// ei system charao onno arek system a kora jai //
+
+// function play(){
+//     // hidden home screen //
+//     const homeSection=document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//     // show playground section //
+//     const playgroundSection =document.getElementById('play-groud')
+//     playgroundSection.classList.remove('hidden');
+// }
+function keyboardKeyupEvent(event) {
+    const playerPresed = event.key;
+    // console.log('player pressed', playerPresed);
+
+    // get expected pressed //
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPresed,expectedAlphabet)
+
+    // cheked match //
+    if (playerPresed === expectedAlphabet) {
+        console.log('you get point')
+        removeBackGroundColorId(expectedAlphabet)
+        continueGame();
+        // current score //
+        const currentScore = getTextElementId('current-score');
+        const newScore = currentScore + 1;
+        const displaytext = setTextId('current-score', newScore)
+        // // get the current score //
+        // const currentScoreElement =document.getElementById('current-score');
+        // const currentScoreText =currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // // console.log(currentScore)
+        // // increase the score //
+        // const newScore =currentScore +1;
+        // // updated score //
+        // currentScoreElement.innerText = newScore;
+    }
+    else {
+        console.log('you missed point');
+        // life score //
+        const currentLife = getTextElementId('current-life');
+        const updatedLife = currentLife - 1;
+        setTextId('current-life', updatedLife)
+
+        if (updatedLife === 0) {
+            gameOver();
+        }
+        // // get the current life //
+        // const currentLifeElement =document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // // get decreased current life //
+        // const newLife = currentLife -1;
+        // // display updated life //
+        // currentLifeElement.innerText = newLife;
+
+    }
+}
+
+document.addEventListener('keyup', keyboardKeyupEvent);
+
+
+function continueGame() {
+    // get random alphabet //
+    const alphabet = getAlphabetRandom();
+    console.log('your alphabet', alphabet)
+
+    // random alphabet show tha display //
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabet;
+
+    // set background color //
+    setBackgroundColorId(alphabet);
+}
+
+function play() {
+    // hide all show only play ground //
+    hiddenElementById('home-screen');
+    shoeElementById('play-ground')
+    hiddenElementById('final-score')
+    // reset life and score //
+    setTextId('current-score',0);
+    setTextId('current-life',5);
+    continueGame()
+}
+
+function gameOver(){
+    hiddenElementById('play-ground');
+    shoeElementById('final-score')
+    // const showFinalscore =document.getElementById('final-score');
+    // showFinalscore.classList.remove('hidden')
+
+    // updated the game over score //
+
+}
+
+
---
+++ universal.js
@@ -0,0 +1,49 @@
+function hiddenElementById(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function shoeElementById(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+function setBackgroundColorId(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+    
+}
+
+function removeBackGroundColorId(elementId){
+    const element =document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+function getTextElementId(elementId){
+    const element = document.getElementById(elementId);
+    const elementTextValue =element.innerText;
+    const textValue= parseInt(elementTextValue);
+    return textValue;
+}
+
+function setTextId(elementId,value){
+    const element =document.getElementById(elementId);
+    element.innerText =value;
+}
+
+
+
+function getAlphabetRandom(){
+    const alphabetString ='abcdefghijklmnopqrstuvwxyz'
+    const alphabets =alphabetString.split('');
+    console.log(alphabets);
+    // get a random index //
+    const randomNumber =Math.random()*25;
+    const randomIndex =Math.round(randomNumber);
+    
+    const alphabet =alphabets[randomIndex];
+    // console.log(randomIndex, alphabet)
+    return alphabet;
+
+}
